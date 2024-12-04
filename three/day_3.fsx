672212bc7665d3eeb21928479268135b323d2b4b
David Barton <david.barton@hyland.com>
2024-12-03 20:33:26 -0600

    combining parts 1 and 2

--- three/day_3.fsx
+++ three/day_3.fsx
@@ -1,7 +1,5 @@
 open System
-open System.Collections.Generic
 open System.IO
-open System.Text
 
 let inputPath = Path.Join(__SOURCE_DIRECTORY__, "input.txt")
 
@@ -18,34 +16,13 @@ let readInt (str: string) =
   else
     Error()
 
-let processStr (inputStr: string) = 
+type MindYourDosAndDonts = Yes | No
+
+let processStr (dos: MindYourDosAndDonts) (inputStr: string)  = 
   let mutable acc = 0
   let mutable idx = 0
   while idx < inputStr.Length - 1 do
-    if inputStr[idx .. idx + 3] = "mul(" then
-      match readInt inputStr[idx + 4..] with 
-      | Ok (first, nextStr) -> 
-        if nextStr[0] = ',' then 
-          match readInt nextStr[1..] with 
-          | Ok (second, nextStr) -> 
-            if nextStr[0] = ')' then 
-              acc <- acc + first * second
-          | Error()-> ()
-      | Error() -> ()
-      idx <- idx + 4
-    else 
-      idx <- idx + 1
-  acc
-
-processStr inputStr
-// part 1 result: 184122457
-
-// part 2
-let processStr2 (inputStr: string) = 
-  let mutable acc = 0
-  let mutable idx = 0
-  while idx < inputStr.Length - 1 do
-    if inputStr[idx .. idx + 6] = "don't()" then 
+    if dos.IsYes && inputStr[idx .. idx + 6] = "don't()" then 
       let mutable j = idx + 6
       while j < inputStr.Length - 1 && inputStr[j .. j + 3] <> "do()" do 
         j <- j + 1
@@ -65,16 +42,11 @@ let processStr2 (inputStr: string) =
       idx <- idx + 1
   acc
 
-"mul(mul(3,2),2)"
+// part 1
+processStr No inputStr
+// part 2
+processStr Yes inputStr
 
-type ExpGrammer = 
-  | Mult of int * int
-  | Integer of int
-  | Trash of string
-  | Do of string
-  | Dont of string
-
-processStr2 inputStr
 
 
 
