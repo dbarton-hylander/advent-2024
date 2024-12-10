0a4eee6039255725e94a8d527fb8f492aa3e4f60
David Barton <david.barton@hyland.com>
2024-12-10 17:45:20 -0600

    adding PSeq to day 7

--- 7/7.fsx
+++ 7/7.fsx
@@ -1,4 +1,7 @@
+#r "nuget: FSharp.Collections.ParallelSeq"
+
 open System.IO
+open FSharp.Collections.ParallelSeq
 
 let rec cartesianProduct size elements =
   if size = 0 then [ [] ]
@@ -18,9 +21,9 @@ let eval (inputFile: string) (ops: (int64 -> int64 -> int64) list)  =
       match line with 
       | result :: numbers -> result, numbers
       | _ -> 0, [])
-  |> Array.filter (fun (result, numbers) ->
+  |> PSeq.filter (fun (result, numbers) ->
       cartesianProduct (numbers.Length - 1) ops
-      |> List.exists (fun operators -> 
+      |> PSeq.exists (fun operators -> 
         numbers
         |> List.fold (fun acc number -> 
           let idx = fst acc
@@ -28,15 +31,21 @@ let eval (inputFile: string) (ops: (int64 -> int64 -> int64) list)  =
           if idx = 0 then (idx + 1, number)
           else (idx + 1, operators[idx - 1] acc number)) (0, 0)
         |> snd = result))
+  |> Array.ofSeq
   |> Array.map fst
   |> Array.sum
 
-// part 1
 eval "example.txt" [ (*); (+) ] // 3749
+
+#time
+// part 1
 eval "input.txt" [ (*); (+) ] // 4998764814652
+#time
 
 let concat a b = int64 $"{a}{b}"
-
-// part 2
 eval "example.txt" [ (*); (+); concat ] // 11387L
+
+#time
+// part 2
 eval "input.txt" [ (*); (+); concat ] // 37598910447546L
+#time
